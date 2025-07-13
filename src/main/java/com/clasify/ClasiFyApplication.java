c07b1739d97637245e32f0c6d4e11b1b739f33d7
Nothing <andy.v.centeno@gmail.com>
2025-07-12 20:06:52 -0500

    Primer commit

---
+++ src/main/java/com/clasify/ClasiFyApplication.java
@@ -0,0 +1,13 @@
+package com.clasify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClasiFyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClasiFyApplication.class, args);
+	}
+
+}
