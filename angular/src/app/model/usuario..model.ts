843853dea74e704b8b493efbc7d9e98e6664b74e
Nothing <andy.v.centeno@gmail.com>
2025-07-12 21:43:16 -0500

    Implementacion de los paquetes, login y crear estudiante (incluye encriptacion)

---
+++ angular/src/app/model/usuario..model.ts
@@ -0,0 +1,10 @@
+export class usuario{
+    idUsuario? :number;
+    nombreUsuario?: string;
+    apellidoUsuario?: string;
+    email?:string;
+    contrasenia?:string;
+    rol?:string;
+    flgEliminado?:boolean;
+    cursosInscritos?: any[];
+}
